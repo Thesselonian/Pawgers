2918e8009b6cf0b5966ae640d7a0aa68cf7c3087
Eric <ericyoung111@gmail.com>
2021-12-13 18:34:06 -0700

    made an endpoint for homepage and created the view for a homepage but it's not very pretty

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api')
-// const homeRoutes = require('./homeroutes')
+const homeRoutes = require('./homeroutes')
 // const dashboardRoutes = require('./dashboard-routes')
 
 router.use('/api', apiRoutes);
-// router.use('/', homeRoutes)
+router.use('/', homeRoutes)
 // router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
