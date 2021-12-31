e90aadaa7f4357bfb15589d58c46a9d0b3cd6ec5
Matt Keys <MatthewjKeys22@gmail.com>
2021-12-31 11:58:40 -0700

    explore page created, barebones.  still work to do

--- controllers/index.js
+++ controllers/index.js
@@ -5,13 +5,14 @@ const homeRoutes = require('./home-routes')
 const dashboardRoutes = require('./dashboard-routes');
 const profileRoutes = require('./profile-routes')
 const adoptionRoutes = require('./adoption-routes');
+const exploreRoutes = require('./explore-routes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes)
 router.use('/dashboard', dashboardRoutes);
 router.use('/profile', profileRoutes);
 router.use('/adoption', adoptionRoutes);
-
+router.use('/explore', exploreRoutes);
 
 
 module.exports = router;
